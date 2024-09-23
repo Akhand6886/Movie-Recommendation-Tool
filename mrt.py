340c6cb39dbb3806a54535a8e5bcc3c1626898b0
akhand6886 <aksingh6886@GMAIL.COM>
2024-09-23 16:21:36 +0530

    main python file

---
+++ mrt.py
@@ -0,0 +1,8 @@
+#Import all necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.html.widgets import *
+sns.set_style('white')
+%matplotlib inline
